439483163dacb38ceb6f459d4320aaaec713210e
hungtran86 <trannguyenhung011086@gmail.com>
2019-11-01 15:31:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const NewsAPI = require('newsapi');
+
+const micro = require('micro');
+const { router, get } = require('micro-fork');
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
+const fetchNews = async (searchTerm, pageNum) => {
+    return await newsapi.v2.everything({
+        q: searchTerm,
+        language: 'en',
+        page: pageNum,
+        pageSize: 5,
+    });
+};
+
+const home = (req, res) => {
+    micro.send(
+        res,
+        200,
+        'Type /topic?search=<term> to get result from News API',
+    );
+};
+
+const fetch = async (req, res) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    const topic = req.query['search'];
+
+    const data = await fetchNews(topic, 1).catch(e => console.log(e));
+
+    micro.send(res, 200, data.articles);
+};
+
+const Router = router()(get('/', home), get('/topic', fetch));
+
+const app = micro(Router);
+
+if (!process.env.IS_NOW) {
+    app.listen(process.env.PORT || 3000);
+    console.log(`Micro is serving at ${process.env.PORT || 3000}`);
+}
+
+module.exports = app;
